c7a75184ee65862a1853a07265a6497e732e275d
Ivo Ventura <ivo.ventura@globalhitss.com.br>
2018-11-20 11:26:20 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,15 +1,17 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api'
+import { Category } from './pages/categories/shared/category.model'
 
 export class InMemoryDatabse implements InMemoryDbService {
 
     createDb(){
 
-        const categories = [
+        const categories: Category[] = [
             {id: 1, name: "Lazer", description: "Ciname, shopping e etc"},
             {id: 2, name: "Compras", description: "Feira, variedades e etc"},
             {id: 3, name: "Financiamento", description: "Apartamento, carro e etc"},
             {id: 4, name: "Cartões", description: "Credcard, Mastercard, Hipercard"},
             {id: 5, name: "Educação", description: "Escola, Faculdade, Idiomas e etc"}
+            
         ]
         return { categories }
     }
